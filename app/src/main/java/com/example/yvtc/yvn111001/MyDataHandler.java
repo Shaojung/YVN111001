16620f46128cb9ec0bdeca583f860cad702add83
shaojung <shaojung@gmail.com>
2016-11-10 20:07:56 +0800

    retrieve data sax parser

---
+++ app/src/main/java/com/example/yvtc/yvn111001/MyDataHandler.java
@@ -0,0 +1,29 @@
+package com.example.yvtc.yvn111001;
+
+import android.util.Log;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+/**
+ * Created by yvtc on 2016/11/10.
+ */
+
+public class MyDataHandler extends DefaultHandler {
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+        super.startElement(uri, localName, qName, attributes);
+        Log.d("NET", qName);
+    }
+
+    @Override
+    public void endElement(String uri, String localName, String qName) throws SAXException {
+        super.endElement(uri, localName, qName);
+    }
+
+    @Override
+    public void characters(char[] ch, int start, int length) throws SAXException {
+        super.characters(ch, start, length);
+    }
+}
