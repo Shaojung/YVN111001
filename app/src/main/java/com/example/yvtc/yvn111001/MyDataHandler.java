8843c6b50ace8f3a966c558402ce0ae40003dc47
shaojung <shaojung@gmail.com>
2016-11-10 20:37:32 +0800

    retrieve data sax parser

--- app/src/main/java/com/example/yvtc/yvn111001/MyDataHandler.java
+++ app/src/main/java/com/example/yvtc/yvn111001/MyDataHandler.java
@@ -11,19 +11,40 @@ import org.xml.sax.helpers.DefaultHandler;
  */
 
 public class MyDataHandler extends DefaultHandler {
+    boolean inTitle, inItem;
     @Override
     public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
         super.startElement(uri, localName, qName, attributes);
-        Log.d("NET", qName);
+        if (qName.equals("title"))
+        {
+            inTitle = true;
+        }
+        if (qName.equals("item"))
+        {
+            inItem = true;
+        }
     }
 
     @Override
     public void endElement(String uri, String localName, String qName) throws SAXException {
         super.endElement(uri, localName, qName);
+        if (qName.equals("title"))
+        {
+            inTitle = false;
+        }
+        if (qName.equals("item"))
+        {
+            inItem = false;
+        }
     }
 
     @Override
     public void characters(char[] ch, int start, int length) throws SAXException {
         super.characters(ch, start, length);
+        if (inTitle & inItem)
+        {
+            String str = new String(ch).substring(start, start + length);
+            Log.d("NET", str);
+        }
     }
 }
