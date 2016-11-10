7e0259f69e66e90d070d943405c140f44ddc9f83
shaojung <shaojung@gmail.com>
2016-11-10 19:19:53 +0800

    Initial commit

---
+++ app/src/main/java/com/example/yvtc/yvn111001/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.yvtc.yvn111001;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+    private InputStream inputStream = null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Thread(){
+            @Override
+            public void run() {
+                super.run();
+
+                String str_url = "http://www.google.com.tw";
+                try {
+                    URL url = new URL(str_url);
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setRequestMethod("GET");
+                    conn.connect();
+                    inputStream = conn.getInputStream();
+
+                    
+
+                    inputStream.close();
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        }.start();
+
+    }
+}
